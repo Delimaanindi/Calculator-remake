e54bfa52700c9c8161db8c05dd99d1d7de457e35
Delimaanindi <delima2730@gmail.com>
2023-01-20 18:35:06 +0700

    converter logic

--- src/components/converter/convert.jsx
+++ src/components/converter/convert.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import './convert.css'
 import {TbArrowsRightLeft} from 'react-icons/tb'
+import { useState } from "react";
 
 export default function Convert() {
+    const [temp, setTemp] = useState('');
+    const [temp2, setTemp2] = useState('');
+    const [result, setResult] = useState('');
+    const [result2, setResult2] = useState('')
+
+    const handleChange = (e) => {
+        if(e.target.name === "celsius") {
+            setTemp(e.target.value)
+        }
+        setTemp2(e.target.value)
+       
+    };
+
+    const converter1 = () => {
+        setResult(((9/5) * temp) + 32).toString();
+        }   
+    const converter2 = () => {
+            setResult2((5/9) * (temp2 -32)).toString();
+        }   
+    
     return(
         <div>
             <h2>Temperature Converter</h2>
@@ -10,26 +31,30 @@ export default function Convert() {
                 <h4 id="title">Celsius to Fahrenheit</h4>
                 <div className="ctof">
                 <div className="input-cont">
-                    <input type="text" placeholder="celcius" className="inputs"/>
+                    <input type="text" placeholder="celcius" className="inputs" name="celsius"
+                    onChange={handleChange}
+                     />
                     </div>
                 <i id="icon"><TbArrowsRightLeft size={30}/></i>
                 <div className="input-cont">
-                    <input type="text" className="inputs" placeholder="fahrenheit"/>
+                    <input type="text" className="inputs" placeholder="fahrenheit" value={result}/>
                     </div>
-                    <button className="convert">Convert</button>
+                    <button className="convert" onClick={converter1}>Convert</button>
                     </div>
             </div>
             <div className="convert-cont2">
             <h4 id="title">Fahrenheit to Celsius</h4>
             <div className="ftoc">
                 <div className="input-cont">
-                    <input type="text" placeholder="fahrenheit" className="inputs"/>
+                    <input type="text" placeholder="fahrenheit" className="inputs" 
+                    name="fahrenheit"
+                    onChange={handleChange}/>
                     </div>
                 <i id="icon"><TbArrowsRightLeft size={30}/></i>
                 <div className="input-cont">
-                    <input type="text" className="inputs" placeholder="celcius"/>
+                    <input type="text" className="inputs" placeholder="celcius" value={result2}/>
                     </div>
-                    <button className="convert">Convert</button>
+                    <button className="convert" onClick={converter2}>Convert</button>
             </div>
             </div>
         </div>
